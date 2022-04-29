6d1db0bf346caff41af09a94c120ce1beaf33bea
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-04-29 23:59:57 +0000

    mise en place d'un projet de Librairie en Line avec Spring Boot

---
+++ Line-Librairie/src/main/java/sn/allit/linelibrairie/LineLibrairieApplication.java
@@ -0,0 +1,13 @@
+package sn.allit.linelibrairie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineLibrairieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineLibrairieApplication.class, args);
+	}
+
+}
---
+++ Line-Librairie/src/test/java/sn/allit/linelibrairie/LineLibrairieApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.allit.linelibrairie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineLibrairieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
